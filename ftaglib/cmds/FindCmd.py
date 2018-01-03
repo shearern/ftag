49f71c79e06f9504f32ad729ba229bdeb6487c62
Nathan Shearer <shearern@gmail.com>
2018-01-02 23:50:39 -0800

    Added --if to find and changed default to not show tags

--- ftaglib/cmds/FindCmd.py
+++ ftaglib/cmds/FindCmd.py
@@ -5,6 +5,7 @@ from .FtagCommand import FtagCommand, FtagCommandArgs
 from ..core.utils import expand_paths
 from ..core import FtagExplorer
 from .. import NoTagDatabaseForPath
+from ..core import FtagFilterExpression
 
 class FindCmd(FtagCommand):
     '''
@@ -16,6 +17,12 @@ class FindCmd(FtagCommand):
     usage = '''\
         tag find (options) paths
         
+        --if filter
+            Show only paths that match a the given filter
+        
+        --show
+            Show the tags each path has
+        
         paths 
             Are the files to add the tags to        
         '''
@@ -26,14 +33,20 @@ class FindCmd(FtagCommand):
         # Init
         args = FtagCommandArgs(
             paths = list(),
+            tagfilter = None,
+            show = False,
         )
 
         # Parse
 
         # Read remaining args
-        for opt in self._read_arguments(argv[1:]):
+        for opt in self._read_arguments(argv[1:], ('--if')):
             if opt.type == 'posarg':
                 args.paths.append(opt.value)
+            elif opt.type == 'flag' and opt.name == '--show':
+                args.show = True
+            elif opt.type == 'flag' and opt.name == '--if':
+                args.tagfilter = FtagFilterExpression(opt.value)
             else:
                 raise UsageError(cmd=self, error="Unknown argument: " + str(opt))
 
@@ -54,6 +67,11 @@ class FindCmd(FtagCommand):
             for dirpath, dirnames, filenames in os.walk(arg_path):
                 for filename in filenames:
                     path = os.path.normpath(os.path.join(dirpath, filename))
-                    print("%s\t[%s]" % (path, ','.join(explorer.get(path).tags)))
+                    tagged = explorer.get(path)
+                    if args.tagfilter is None or args.tagfilter.matches(tagged):
+                        if args.show:
+                            print("%s\t[%s]" % (path, ','.join(tagged.tags)))
+                        else:
+                            print(path)
 
         explorer.close_dbs()
